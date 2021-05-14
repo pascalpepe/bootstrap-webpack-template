df06c7e39f47304a3f77bde69efa3bb25486be2f
Pascal Pepe <contact@pascalpepe.com>
2021-05-14 15:07:38 +0200

    Update Bootstrap to version 5

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 /*!
- * Bootstrap v4.6.0 (https://getbootstrap.com/)
+ * Bootstrap v5.0.1 (https://getbootstrap.com/)
  * Copyright 2011-2021 The Bootstrap Authors
  * Copyright 2011-2021 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
  */
 
-// Required
-import 'bootstrap/js/dist/util';
-
-// Optional
+// Components (optional)
 import 'bootstrap/js/dist/alert';
 import 'bootstrap/js/dist/button';
 import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
 import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/offcanvas';
 import 'bootstrap/js/dist/popover';
 import 'bootstrap/js/dist/scrollspy';
 import 'bootstrap/js/dist/tab';
